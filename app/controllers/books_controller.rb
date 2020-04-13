5b4f711bbba358b9ddefba65b6785761f0c0914e
Ben-1327 <ff.walker1327@gmail.com>
2020-04-14 00:14:50 +0900

    200413

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,6 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    @book = Book.new
   end
 
   def show
@@ -13,15 +12,29 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def create
     book = Book.new(book_params)
     book.save
     redirect_to books_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resource :books, :except => [:index, :create, :new, :show]
+  resource :books, :except => [:index, :create, :new, :show, :edit, :destroy]
   root :to => 'books#top'
   get 'books' => 'books#index'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
