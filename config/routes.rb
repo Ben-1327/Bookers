bbb07d42da7d51b01d1abd8eaa49d424c92fb4fa
Ben-1327 <ff.walker1327@gmail.com>
2020-04-14 18:24:17 +0900

    200414 1830

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  resource :books, :except => [:index, :create, :new, :show, :edit, :destroy]
+  resources :books, :except => [:index, :create, :new]
   root :to => 'books#top'
   get 'books' => 'books#index'
   post 'books' => 'books#create'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
